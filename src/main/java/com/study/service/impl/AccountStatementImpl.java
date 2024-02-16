ae61685fcc943cd042b5a913a96292406782ec48
Gabriel Rodrigues dos Santos <ga_biel@msn.com>
2024-02-16 11:35:48 -0300

    Create AccountStatementImpl.java

---
+++ src/main/java/com/study/service/impl/AccountStatementImpl.java
@@ -0,0 +1,14 @@
+package com.study.service.impl;
+
+import com.study.model.Statement;
+import com.study.service.AccountStatement;
+
+public class AccountStatementImpl implements AccountStatement {
+
+	@Override
+	public Statement statement(Long accountId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
